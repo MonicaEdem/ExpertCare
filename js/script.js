6be7c7104ab3ca20f7b725705a349ba1bd989dc5
Monica Edem Kokovena <monicaekokovena@gmail.com>
2024-06-14 09:57:44 +0000

    polishing

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ packages.onclick = function ()
 {  alert('We have many exciting packages just for YOU!');
 }
 
-// const headerbtn1 = document.getElementById('button2');
-// headerbtn1.onclick = function () 
-// {  alert('By clicking OK, this link is redirecting you to apple store');
-// }
+const about = document.getElementById('about');
+about.onclick = function () 
+{  alert('Get to know us and about our services');
+}
